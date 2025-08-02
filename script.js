3ee5017565443f649564621c1e8a5ab17be66f0e
DSAwithCode <dsawithcode00@gmail.com>
2025-08-02 01:06:48 -1200

    Coupan Feature

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const button = document.getElementById("btn");
+const buttonSecond = document.getElementById("second");
 button.addEventListener("click",()=>{
     alert("Cart Feature")
+})
+buttonSecond.addEventListener("click",()=>{
+    alert("Coupan Code ")
 })
